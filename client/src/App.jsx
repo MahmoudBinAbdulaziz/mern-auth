c6654d65478bb503f5ebd35058a43ca6ac540e74
Mahmoud <mahmoud.abdulazizz.1911@gmail.com>
2025-04-20 20:51:20 +0200

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,17 @@
+import { About, SignIn, SignUp, Profile, Home } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  return <>hi</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sgn-in" element={<SignIn />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
