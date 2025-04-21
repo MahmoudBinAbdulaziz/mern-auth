32e5d210aa2e10ae955917d91b62768291e5c062
Mahmoud <mahmoud.abdulazizz.1911@gmail.com>
2025-04-21 08:59:43 +0200

    connect to db

--- api/app.js
+++ api/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 
+mongoose
+  .connect(process.env.MONGOOSE_URI)
+  .then(() => {
+    console.log("conect to db");
+  })
+  .catch((err) => console.log(err));
 app.listen(3000, () => {
   console.log("server running at port " + 3000);
 });
