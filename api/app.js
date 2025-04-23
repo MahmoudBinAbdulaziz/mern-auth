1752def31f8089045920937878b218e3d3d9811c
Mahmoud <mahmoud.abdulazizz.1911@gmail.com>
2025-04-23 22:22:28 +0200

    fix to roduction

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ const userRouter = require("./routs/userRoute");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routs/AuthRoute");
 require("dotenv").config();
+import path from "path";
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
@@ -18,7 +19,13 @@ mongoose
 app.listen(3000, () => {
   console.log("server running at port " + 3000);
 });
+const __dirname = path.resolve();
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
